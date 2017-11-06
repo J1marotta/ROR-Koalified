a22f61d060f8dbd3e4c9fd97b066e8c8ca7c5542
J1marotta <jim_is55@hotmail.com>
2017-11-06 13:07:55 +1100

    Removed Fb login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -250,8 +250,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-   config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-   info_fields: 'email,first_name,last_name' , scope: 'email,public_profile', display: 'popup', callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  #  config.omniauth :facebook, 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  authenticated :user do
+    root 'home#index', as: 'authenticated_root'
+  end
 
-    root to: 'home#index'
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-
-# devise_scope :user do
-#   get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
-#   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-# end
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
 end
