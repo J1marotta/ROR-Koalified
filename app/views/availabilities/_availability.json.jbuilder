45487504c41dce7af85c8323d9f4d5216d54c78e
J1marotta <jim_is55@hotmail.com>
2017-11-07 16:26:40 +1100

    Fix driver availbilities form

--- app/views/availabilities/_availability.json.jbuilder
+++ app/views/availabilities/_availability.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! availability, :id, :price, :day, :length, :booked, :user_id, :created_at, :updated_at
+json.extract! availability, :id, :price, :day, :start_time, :booked, :user_id, :created_at, :updated_at
 json.url availability_url(availability, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107030448) do
+ActiveRecord::Schema.define(version: 20171107045026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "availabilities", force: :cascade do |t|
     t.string "day"
-    t.time "length"
+    t.time "start_time"
     t.boolean "booked"
     t.bigint "user_id"
     t.datetime "created_at", null: false
